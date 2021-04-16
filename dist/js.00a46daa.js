49c4f7e3b8b64006f9a188aad3b7c44df54d95c9
reyfun1 <reifundora@gmail.com>
2021-04-16 16:42:55 -0600

    minified images, fixed alt missing isssue

--- dist/js.00a46daa.js
+++ dist/js.00a46daa.js
@@ -327,7 +327,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62120" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58507" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
